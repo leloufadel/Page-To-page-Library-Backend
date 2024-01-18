f5288deab13278a9962b3dac5d1f11fd1f909f04
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-18 20:04:49 +0430

    add the validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   has_many :book_reservations
   has_many :reservations, through: :book_reservations
+
+  validates :name, presence: true
+  validates :cover_photo, presence: true
+  validates :author, presence: true
+  validates :publisher, presence: true
+  validates :publish_date, presence: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,8 @@ class Reservation < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   has_many :book_reservations
   has_many :books, through: :book_reservations
+
+  validates :date, presence: true
+  validates :city, presence: true
+  validates :due_date, presence: true
 end

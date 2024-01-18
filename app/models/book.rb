d3a563947d9fbb9901298d71aac2f2d2c534febc
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-18 22:04:03 +0200

    Modify model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,9 @@ class Book < ApplicationRecord
   has_many :book_reservations
   has_many :reservations, through: :book_reservations
 
-  validates :name, presence: true
-  validates :cover_photo, presence: true
-  validates :author, presence: true
-  validates :publisher, presence: true
-  validates :publish_date, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :cover_photo, presence: true, length: { minimum: 3 }
+  validates :author, presence: true, length: { minimum: 3 }
+  validates :publisher, presence: true, length: { minimum: 3 }
+  validates :publish_date, presence: true, length: { minimum: 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :books, foreign_key: :user_id
   has_many :reservations, foreign_key: :user_id
 
-  validates :name, presence: true
-  validates :password, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 6 }
 end

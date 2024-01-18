b1794e9fd640cacfdd5a9129aebc78873ed722db
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-18 22:04:46 +0200

    Create destroy and show methods for api

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -4,11 +4,27 @@ class Api::BooksController < ApplicationController
     render json: @books
   end
 
-  def show; end
+  def show
+    @book = Book.find_by(id: params[:id])
+    
+    if @book
+      render json: @book, status: :ok
+    else
+      render json: { message: 'Book not found' }, status: :bad_request
+    end
+  end
 
   def create; end
 
-  def destroy; end
+  def destroy
+    @books = Book.find_by(id: params[:id])
+
+    if @books.destroy
+      render json: { message: 'Book Was deleted successfully' }, status: :ok
+    else
+      render json: { message: 'Something went wrong'}, status: :not_found
+    end
+  end
 
   private
 

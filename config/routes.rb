2078d59ca1e290b3edc7d0212f23bd09fa786ed5
Arch-Noize <santiagonoize@gmail.com>
2024-01-18 09:51:43 -0400

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'reservations/create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

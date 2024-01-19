cab69e979521be937604a80c52c36290de9530da
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-19 17:21:26 +0300

    Update database with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :jwt_authenticatable,
+  jwt_revocation_strategy: JwtDenylist
+
   has_many :books, foreign_key: :user_id
   has_many :reservations, foreign_key: :user_id
 

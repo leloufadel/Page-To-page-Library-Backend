3c55ed6e952ef265cf16381433fc92cc1f46862a
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-19 20:14:16 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/member-data', to: 'members#show'
+
   namespace :api do
     resources :books, only: [:index, :show, :create, :destroy]
     resources :reservations

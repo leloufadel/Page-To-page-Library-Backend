0dd273fab8152253ca2ee4bfda9a446b01452e64
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-20 06:35:41 +0300

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :jwt_authenticatable,
-  jwt_revocation_strategy: JwtDenylist
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
 
   has_many :books, foreign_key: :user_id
   has_many :reservations, foreign_key: :user_id

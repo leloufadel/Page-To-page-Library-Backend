f49e9412794eca449d51f7765f1ed581f25f074f
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-20 12:59:10 +0300

    Update the code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      resource '*', headers: :any, methods: [:get, :post, :patch, :put],
+      expose: %w[Authorization Uid]
     end
 end

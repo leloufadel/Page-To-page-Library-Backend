ee9d48f77fe947e081e6450d29835a7663fed385
leloufadel <leloufadel@gmail.com>
2024-01-21 15:34:39 +0000

    add columns to table books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_100619) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_153207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_100619) do
     t.date "publish_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.text "info"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|

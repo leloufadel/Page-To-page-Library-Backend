9a5224f0165ab893ca5dd3f065e4738089093710
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-21 21:48:18 +0430

    add the admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
     user ||= User.new
-    # guest user (not logged in)
+
     if user.admin?
       can :manage, :all
     else
       can :read, :all
+      can %i[destroy create], Reservation, user_id: user.id
     end
   end
 end

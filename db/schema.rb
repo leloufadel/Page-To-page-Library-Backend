f195322cf7728998a4741bd4af82c00f8b4f31ae
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-21 23:02:44 +0300

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_100619) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false

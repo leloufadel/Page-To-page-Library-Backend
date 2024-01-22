eb7940251da9590872936a8f4bf274d459b37eee
Arch-Noize <santiagonoize@gmail.com>
2024-01-22 05:43:47 -0400

    Update seeds with new info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,22 @@
-User.create(name: "Sadaf", password:"654321",email:"sadaf@gmail.com")
-User.create(name: "Garvin", password:"123456",email:"abc@gmail.com")
-Book.create(name: "The Alchemist", cover_photo:"test.png", author:"THE Author", publisher:"One", publish_date:"2021-01-01")
-Book.create(name: "The Magician", cover_photo:"test3.png", author:"THE Writer", publisher:"One", publish_date:"2021-01-01")
-Book.create(name: "The Doctor", cover_photo:"test2.png", author:"THE Medic", publisher:"One", publish_date:"2021-01-01")
+Book.create(name: "The Alchemist", 
+            cover_photo:"test.png", 
+            author:"THE Author", 
+            publisher:"One", 
+            publish_date:"2021-01-01", 
+            category:"Fiction", 
+            info:"This is a testing side of info")
+Book.create(name: "The Magician", 
+            cover_photo:"test3.png", 
+            author:"THE Writer",
+            publisher:"One", 
+            publish_date:"2021-01-01", 
+            category:"Fiction", 
+            info:"This is a testing side of info")
+Book.create(name: "The Doctor", 
+            cover_photo:"test2.png", 
+            author:"THE Medic",
+            publisher:"One", 
+            publish_date:"2021-01-01", 
+            category:"Fiction", 
+            info:"This is a testing side of info")
 Reservation.create(date: "2000-10-19", due_date: "2000-10-29", city: "Caracas", user_id: 1)

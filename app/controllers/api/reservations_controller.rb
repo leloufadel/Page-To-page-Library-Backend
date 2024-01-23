b62d7cf67ba23c8e8be562ae747b2a33ed614479
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-23 22:20:41 +0430

    resolve the rubocop error

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,14 +1,17 @@
 class Api::ReservationsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @reservations = @user.reservations
-    render json: @reservations
+    @reservations = @user.reservations.includes(:books)
+    render json: @reservations.as_json(include: :books)
   end
 
   def create
     @user = User.find(params[:user_id])
     @reservation = @user.reservations.build(reservation_params)
     if @reservation.save
+      params[:book_ids].each do |book_id|
+        BookReservation.create(book_id:, reservation_id: @reservation.id)
+      end
       render json: @reservation, status: :created, location: api_reservation_url(@reservation)
     else
       render json: @reservation.errors, status: :unprocessable_entity

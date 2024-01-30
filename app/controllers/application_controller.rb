efed0d83774b95276e40fd0c332707f267963848
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-30 12:26:16 +0300

    Update code with role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation name role])
   end
 end

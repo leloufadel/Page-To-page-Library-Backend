65ea59c23ea396ec3bc8b84274cfc4e9aa3ab968
Arch-Noize <santiagonoize@gmail.com>
2024-01-31 07:15:10 -0400

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
-./bin/rails db:drop
-./bin/rails db:create
+./bin/rails db:reset
 ./bin/rails db:migrate
+./bin/rails runner "ActiveRecord::Base.connection.tables.each { |t| ActiveRecord::Base.connection.execute('TRUNCATE TABLE ' + t + ' CASCADE') unless ['schema_migrations', 'ar_internal_metadata'].include?(t) }"
 ./bin/rails db:seed
